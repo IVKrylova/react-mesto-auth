270960442ac261378cb13b199cd3dab13b87c09e
IVKrylova <ivk.lis@yandex.ru>
2022-03-23 14:13:35 +0300

    refactor: добавить basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basemane="/react-mesto-auth/">
+    <BrowserRouter basemane="/react-mesto-auth">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
