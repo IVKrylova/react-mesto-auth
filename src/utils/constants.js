0e5884ae2a654aa13d0508e49fe789b547c2ba51
IVKrylova <ivk.lis@yandex.ru>
2022-05-05 22:04:06 +0300

    change BASE_URL for authorization

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 // базовый URL для регистрации/авторизации
-export const BASE_URL = 'https://auth.nomoreparties.co';
+// export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://api.mesto.ivkrylova.nomoredomains.xyz';
 
 // объект с настройками для API
 export const options = {
