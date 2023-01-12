c34ca67991187742e4c2c0fa4073f355ed88dbdf
IVKrylova <ivk.lis@yandex.ru>
2023-01-12 23:51:15 +0300

    fix: change my api options

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,9 @@
 // базовый URL для регистрации/авторизации
-export const BASE_URL = 'https://api.mesto.ivkrylova.nomoredomains.xyz';
+export const BASE_URL = 'https://api.ivkrylova-mesto.fun';
 
 // объект с настройками для API
 export const options = {
-  baseUrl: 'https://api.mesto.ivkrylova.nomoredomains.xyz',
+  baseUrl: 'https://api.ivkrylova-mesto.fun',
   headers: {
     'Content-Type': 'application/json'
   }
